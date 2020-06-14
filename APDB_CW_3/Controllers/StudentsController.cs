0d7df734c560d1bd12c83412d7ce5b8dca0b1188
Marek Lewandowski <marek@survicate.com>
2020-06-14 02:24:37 +0200

    Koniec zadanie 4

--- APDB_CW_3/Controllers/StudentsController.cs
+++ APDB_CW_3/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using APDB_CW_3.DAL;
 using APDB_CW_3.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -10,17 +12,59 @@ namespace APDB_CW_3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        private readonly IDbService _dbService;
-
-        public StudentsController(IDbService dbService)
-        {
-            _dbService = dbService;
-        }
-
         [HttpGet]
         public IActionResult GetStudents(string orderBy)
         {
-            return Ok(_dbService.GetStudents());
+            List<Student> students = new List<Student>();
+            using (var con =
+                new SqlConnection("Data Source=127.0.0.1,1433;Database=master;User Id=sa;Password=password1337;"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM dbo.Student";
+                con.Open();
+                var reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    students.Add(new Student(
+                        reader["FirstName"].ToString(),
+                        reader["LastName"].ToString(),
+                        reader["IndexNumber"].ToString(),
+                        int.Parse(reader["IdEnrollment"].ToString()),
+                        DateTime.Parse(reader["BirthDate"].ToString())
+                    ));
+                }
+            }
+
+            return Ok(students);
+        }
+
+        [HttpGet("{IndexNumber}/enrollments")]
+        public IActionResult GetEnrollments(string IndexNumber)
+        {
+            List<Enrollment> enrollments = new List<Enrollment>();
+            using (var con =
+                new SqlConnection("Data Source=127.0.0.1,1433;Database=master;User Id=sa;Password=password1337;"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText =
+                    "SELECT E.* FROM dbo.Enrollment E JOIN dbo.Student S ON S.IdEnrollment = E.IdEnrollment WHERE S.IndexNumber = @IndexNumber";
+                com.Parameters.AddWithValue("IndexNumber", IndexNumber);
+                con.Open();
+                var reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    enrollments.Add(new Enrollment(
+                        int.Parse(reader["IdEnrollment"].ToString()),
+                        int.Parse(reader["Semester"].ToString()),
+                        int.Parse(reader["IdStudy"].ToString()),
+                        DateTime.Parse(reader["StartDate"].ToString())
+                    ));
+                }
+            }
+
+            return Ok(enrollments);
         }
 
         [HttpGet("{id}")]
--- APDB_CW_3/Models/Student.cs
+++ APDB_CW_3/Models/Student.cs
@@ -1,17 +1,22 @@
+using System;
+
 namespace APDB_CW_3.Models
 {
     public class Student
     {
-        public Student(int idStudent, string firstName, string lastName)
+        public Student(string firstName, string lastName, string indexNumber, int idEnrollment, DateTime birthDate)
         {
-            IdStudent = idStudent;
             FirstName = firstName;
             LastName = lastName;
+            IndexNumber = indexNumber;
+            IdEnrollment = idEnrollment;
+            BirthDate = birthDate;
         }
 
-        public int IdStudent { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string IndexNumber { get; set; }
+        public int IdEnrollment { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
